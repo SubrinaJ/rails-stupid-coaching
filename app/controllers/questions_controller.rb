b8034762c0cc44387c61117340fd40ce08ca5a65
Subrina Joseph <subrinajoseph@hotmail.com>
2022-03-07 14:44:29 -0400

    Finish exo Stupid Coaching

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,16 @@
 class QuestionsController < ApplicationController
   def ask
+  end
 
+  def answer
+    @ask = ''
+
+    if params[:ask] =='I am going to work'
+      @answer = 'Great'
+    elsif params[:ask].include?("?")
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, you get dressed and go to work."
+    end
   end
 end
